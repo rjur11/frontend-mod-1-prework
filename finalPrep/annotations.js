738eafeb9d681d3970c0c57ae944935f0a42127d
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-09-07 11:39:08 -0400

    Add annotations to prework

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -2,10 +2,12 @@
 // Use the double // to create a new comment
 
 //Build a Bear
+// Create function buildABear that takes in the args name, age, fur, clothes, and specialPower
 function buildABear(name, age, fur, clothes, specialPower) {
-  var greeting = `Hey partner! My name is ${name} - will you be my friend?!`;
-  var demographics = [name, age];
-  var powerSaying = "Did you know that I can " + specialPower + " ?";
+  var greeting = `Hey partner! My name is ${name} - will you be my friend?!`; // Assign variable greeting to string "Hey partner, my name is (arg name)- will you be my friend?"
+  var demographics = [name, age]; // Assign variable demographics to an array containing name and age
+  var powerSaying = "Did you know that I can " + specialPower + " ?"; // Assign variable powerSaying to string "Did you know that I can" concatenated with var specialPower and "?"
+  // Create object builtBear that assigns the key value pairs of: basicInfo (demographics array), clothes to arg clothes, exterior to arg fur, fixed cost of 49.99, sayings (array of greeting, powerSaying and "Goodnight my friend"), and isCuddly(bool set to true)
   var builtBear = {
     basicInfo: demographics,
     clothes: clothes,
@@ -14,29 +16,37 @@ function buildABear(name, age, fur, clothes, specialPower) {
     sayings: [greeting, powerSaying, "Goodnight my friend!"],
     isCuddly: true,
   };
-
+  // Return out buildBear values (do not print out)
   return builtBear
 }
 
+// Using above function, create 2 new bears
 buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares');
 buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in');
 
 
 
 //FizzBuzz
+// Create new function FizzBuzz that takes in 2 nums and a range
 function fizzBuzz(num1, num2, range) {
+  // Iterate through our range
   for (var i = 0; i <= range; i++) {
+    // If num1 and num2 are even, log "fizzbuzz"
     if (i % num1 === 0 && i % num2 === 0) {
       console.log('fizzbuzz');
+      // if num1 is even, log "fizz"
     } else if (i % num1 === 0) {
       console.log('fizz');
+      // if num2 is even, log "buzz"
     } else if (i % num2 === 0) {
       console.log('buzz');
+      // if we get anything other than the above cases, log the number
     } else {
       console.log(i);
     }
   }
 }
 
-fizzBuzz(3, 5, 100);
+// Call function fizzBuzz twice with two sets of args
+fizzBuzz(3, 5, 100); 
 fizzbuzz(5, 8, 400);
